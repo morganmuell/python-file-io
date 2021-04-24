87a963feb92f686adcce1bf21c125a4ad25e2f79
Morgan Muell <morganrmuell@gmail.com>
2021-04-24 16:55:02 -0500

    Script with added frills from best practices. Doesn't quite work but full disclosure I'm running out of time. Previous push of the script works as it is supposed to. sorry :(

--- origin.py
+++ origin.py
@@ -1,28 +1,74 @@
+#! /usr/bin/env python3
+
+"A module for searching text files for words"
+
+import sys
 import re
 
-print('Opening origin.txt')
-
-line_number = 0
-inherit_bait_string = r'inherit[a-z]+'
-inherit_string = re.compile(inherit_bait_string, re.IGNORECASE)
 
 
-with open('origin.txt', 'r') as in_stream:
-    print('Opening output_origin.txt')
+def search_text_for_strings(textfile, string):
+    """
+    Searches a text file for a given search string and writes results to an output file.
 
-    with open('output_origin.txt', 'w') as out_stream:
+    Parameters
+    ----------
+    textfile: textIO
+        The text file to search.
 
-        for line in in_stream:
-            line = line.strip()
-            word_list = line.split()
-            matched_words_inherit = list(filter(inherit_string.findall, word_list))
-            for word in matched_words_inherit:
-                out_stream.write('{0}\t{1}\n'.format(line_number, word))
-            line_number += 1
+    string: string
+            The search string to look for. Can be a regular expression.
+            It is best to enter your expression within r'' to use a raw string.
+    
+    Returns
+    -------
+    output file
+        Output file with hits. One hit per line, listing the line number and the hit.
+
+    Examples
+    --------
+    >>> search_text_for_strings(origin.txt, r'inherit[a-z]+')
+    input file is closed?
+    output file is closed?
+
+    >>> search_text_for_strings(origin.txt, r'inheritable')
+    input file is closed?
+    output file is closed?
+
+    The first example returns all variations on the word heritable, while the second returns only the word heritable.
+    """
 
 
-print("done!")
-print('origin.txt is closed?', in_stream.closed)
-print('output_origin.txt is closed?', out_stream.closed)
+    line_number = 0
+    bait_string = string
+    re_string = re.compile(bait_string, re.IGNORECASE)
 
 
+    with open(textfile, 'r') as in_stream:
+        print('Opening' textfile)
+
+        with open('output_search.txt', 'w') as out_stream:
+
+            for line in in_stream:
+                line = line.strip()
+                word_list = line.split()
+                matched_words = list(filter(re_string.findall, word_list))
+                for word in matched_words:
+                    out_stream.write('{0}\t{1}\n'.format(line_number, word))
+                line_number += 1
+
+
+    print('input file is closed', in_stream.closed)
+    print('output file is closed', out_stream.closed)
+
+
+
+
+
+if __name_+ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting the following command lines in order: text file, search string")
+
+    search_file = search_text_for_strings(textfile, string)
+
+    print('Search complete')
