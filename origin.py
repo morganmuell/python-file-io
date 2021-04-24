44ec642e427c9781627e5a469e157255f679b8d2
Morgan Muell <morganrmuell@gmail.com>
2021-04-24 15:14:41 -0500

    Working code, lacking functions modules and docstrings

--- origin.py
+++ origin.py
@@ -1,21 +1,28 @@
+import re
+
 print('Opening origin.txt')
+
+line_number = 0
+inherit_bait_string = r'inherit[a-z]+'
+inherit_string = re.compile(inherit_bait_string, re.IGNORECASE)
+
+
 with open('origin.txt', 'r') as in_stream:
     print('Opening output_origin.txt')
+
     with open('output_origin.txt', 'w') as out_stream:
+
         for line in in_stream:
-            #variable for line number
-            #parse apart words for that line (with line.strip() and line.split ()?)
-            #search for the related words
-            #store search results of words into another variable, separated by a comma and space
-            for word in word_list:
-                out_stream.write('{0}\n'.format(word))
-                #also write the line number first
+            line = line.strip()
+            word_list = line.split()
+            matched_words_inherit = list(filter(inherit_string.findall, word_list))
+            for word in matched_words_inherit:
+                out_stream.write('{0}\t{1}\n'.format(line_number, word))
+            line_number += 1
+
 
 print("done!")
 print('origin.txt is closed?', in_stream.closed)
-print('output_origin.txt is closed?', out.stream.closed)
+print('output_origin.txt is closed?', out_stream.closed)
 
-#search for the words in the line
-#output line number and the word
-#need variable for line number
 
